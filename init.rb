fc377c7eb783ed0dab3086cfb79dd50b5c7d20e6
Akira Saito <a-saito@bpm-gr.co.jp>
2019-02-10 17:41:45 +0900

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_plugin_template do
+  name 'Redmine Plugin Template plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
